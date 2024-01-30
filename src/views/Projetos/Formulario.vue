910fad0da7acbd898c6b0abdffa7eee7542f0e47
Michel Klein <michel.silva@eduzz.com.br>
2024-01-30 10:55:52 -0300

    feat: edit project

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -24,6 +24,17 @@ import { useStore } from '@/store';
 
 export default defineComponent({
   name: 'FormularioView',
+  props: {
+    id: {
+      type: String,
+    },
+  },
+  mounted() {
+    if (this.id) {
+      const projeto = this.store.state.projetos.find((proj) => proj.id === this.id);
+      this.nomeDoProjeto = projeto?.nome || '';
+    }
+  },
   data() {
     return {
       nomeDoProjeto: '',
@@ -31,7 +42,14 @@ export default defineComponent({
   },
   methods: {
     salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto);
+      if (this.id) {
+        this.store.commit('ALTERA_PROJETO', {
+          id: this.id,
+          nome: this.nomeDoProjeto,
+        });
+      } else {
+        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto);
+      }
       this.nomeDoProjeto = '';
       this.$router.push('/projetos');
     },
