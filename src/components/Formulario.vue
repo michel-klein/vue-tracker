b46d0a03d37354962e9a2a06e676149e8fcb6333
Michel Klein <michel.silva@eduzz.com.br>
2024-01-30 10:36:41 -0300

    feat: vuex store

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,12 +2,26 @@
 <template>
   <div class="box formulario">
     <div class="columns">
-      <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+      <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
         <input
         type="text"
         class="input"
         placeholder="Qual tarefa você deseja iniciar?"
         v-model="descricao">
+      </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="projeto.id"
+              v-for="projeto in projetos"
+              :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
       </div>
       <div class="column">
         <Temporizador @ao-temporizador-finalizado="finalizarTarefa" />
@@ -17,7 +31,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useStore } from 'vuex';
+import { key } from '@/store';
 import Temporizador from './Temporizador.vue';
 
 export default defineComponent({
@@ -25,6 +41,7 @@ export default defineComponent({
   data() {
     return {
       descricao: '',
+      idProjeto: '',
     };
   },
   methods: {
@@ -32,9 +49,16 @@ export default defineComponent({
       this.$emit('aoSalvarTarefa', {
         duracaoEmSegundos: tempoDecorrido,
         descricao: this.descricao,
+        projeto: this.projetos.find((projeto) => projeto.id === this.idProjeto),
       });
       this.descricao = '';
     },
+  },
+  setup() {
+    const store = useStore(key);
+    return {
+      projetos: computed(() => store.state.projetos),
+    };
   },
   components: { Temporizador },
   emits: ['aoSalvarTarefa'],
--- src/interfaces/ITarefa.ts
+++ src/interfaces/ITarefa.ts
@@ -1,5 +1,8 @@
+import IProjeto from './IProjeto';
+
 /* eslint-disable semi */
 export default interface ITarefa {
   duracaoEmSegundos: number,
   descricao: string,
+  projeto: IProjeto,
 }
