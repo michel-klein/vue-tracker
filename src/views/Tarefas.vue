3d12fe36b5942ced1a2ea17e5f138d300d153793
Michel Klein <michel.silva@eduzz.com.br>
2024-01-31 14:43:44 -0300

    refactor: modal component

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -20,32 +20,29 @@
     <Box v-if="!tarefas.length">
     Você ainda não realizou nenhuma tarefa!
     </Box>
-    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Editar tarefa</p>
-          <button class="delete" aria-label="close" @click="fecharModal"></button>
-        </header>
-        <section class="modal-card-body">
-          <div class="field">
-            <label for="descricaoDaTarefa" class="label">
-            Descrição da tarefa
-            </label>
-            <input
-            type="text"
-            class="input"
-            v-model="tarefaSelecionada.descricao"
-            id="descricaoDaTarefa"
-            >
-          </div>
-        </section>
-        <footer class="modal-card-foot">
+    <Modal :mostrar="tarefaSelecionada !== null" v-if="tarefaSelecionada">
+      <template v-slot:cabecalho>
+        <p class="modal-card-title">Editar tarefa</p>
+        <button class="delete" aria-label="close" @click="fecharModal"></button>
+      </template>
+      <template v-slot:corpo>
+        <div class="field">
+          <label for="descricaoDaTarefa" class="label">
+          Descrição da tarefa
+          </label>
+          <input
+          type="text"
+          class="input"
+          v-model="tarefaSelecionada.descricao"
+          id="descricaoDaTarefa"
+          >
+        </div>
+      </template>
+        <template v-slot:rodape>
           <button class="button is-success" @click="alterarTarefa">Salvar alterações</button>
           <button class="button" @click="fecharModal">Cancelar</button>
-        </footer>
-      </div>
-    </div>
+        </template>
+    </Modal>
   </div>
 </template>
 
@@ -58,6 +55,7 @@ import {
   ALTERAR_TAREFA, CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS,
 } from '@/store/tipo-acoes';
 import ITarefa from '@/interfaces/ITarefa';
+import Modal from '@/components/Modal.vue';
 import Formulario from '../components/Formulario.vue';
 import Tarefa from '../components/Tarefa.vue';
 import Box from '../components/Box.vue';
@@ -104,7 +102,10 @@ export default defineComponent({
     };
   },
   components: {
-    Formulario, Tarefa, Box,
+    Formulario,
+    Tarefa,
+    Box,
+    Modal,
   },
 });
 </script>
